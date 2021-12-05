27b6d638b3d5c19458bc1f56c3385e4ccd90ff19
hegunhee <gunhee0072@gmail.com>
2021-12-05 20:05:31 +0900

    fcm으로 받아온 데이터를 가지고 Notification을 만들어보았습니다. 다음에는 타입으로 나누어서 동작하는걸 구현해보겠습니다.

--- app/src/main/java/com/hegunhee/fcmstudy/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/hegunhee/fcmstudy/MyFirebaseMessagingService.kt
@@ -1,5 +1,9 @@
 package com.hegunhee.fcmstudy
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
@@ -25,11 +29,64 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             Log.d("타이틀: ",data["title"] ?: "타이틀 찾지못함")
             Log.d("메세지: ",data["message"] ?: "메세지도 찾지못함")
             Log.d("바디?","hello2 ")
-            sendNotification(remoteMessage)
+
+
+            createNotificationChannel()
+
+            val type = remoteMessage.data["type"]?.let { NotificationType.valueOf(it) }
+            val title = remoteMessage.data["title"]
+            val message = remoteMessage.data["message"]
+            type ?: return
+
+
+
+            NotificationManagerCompat.from(this)
+                .notify(type.id,createNotification(type,title,message))
         }else{
             Log.d("수신에러: ","data가 비어있습니다. 메시지를 수신하지 못했습니다.")
             Log.d("data 값: ", remoteMessage.data.toString())
         }
+    }
+
+    private fun createNotification(type : NotificationType,title : String?, message : String?): Notification{
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        when(type){
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "BIGSTYLE"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+
+            }
+        }
+        return notificationBuilder.build()
+    }
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_NAME
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+    companion object{
+        const val CHANNEL_ID = "id"
+        const val CHANNEL_NAME = "name"
+        const val CHANNEL_DESC = "채널 설명입니다."
     }
 
     private fun sendNotification(remoteMessage: RemoteMessage){
