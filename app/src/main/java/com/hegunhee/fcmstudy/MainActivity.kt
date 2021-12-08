e8e35a6c3c90443f51a48fed231416b43b5eaec3
hegunhee <gunhee0072@gmail.com>
2021-12-08 18:05:09 +0900

    notification을 만들어보고 타입별로 분류 pendingIntent를 사용했습니다.

--- app/src/main/java/com/hegunhee/fcmstudy/MainActivity.kt
+++ app/src/main/java/com/hegunhee/fcmstudy/MainActivity.kt
@@ -1,30 +1,58 @@
 package com.hegunhee.fcmstudy
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.hegunhee.fcmstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.resultTextView)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.firebaseTokenTextView)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener {task ->
-            if (!task.isSuccessful) {
-                Log.d("Test", "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
+        initFirebase()
+        updateResult()
+    }
+
+    private fun initFirebase() {
+        FirebaseMessaging.getInstance().token
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
+                }
             }
+    }
 
-            // Get new FCM registration token
-            val token = task.result
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
 
-            // Log and toast
-            binding.tokenText.text = token ?: "토큰이 없읍니다"
-            Log.d("test", binding.tokenText.text.toString())
-        })
+        setIntent(intent)
+        updateResult(true)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런처") +
+                if (isNewIntent) {
+                    "(으)로 갱신했습니다."
+                } else {
+                    "(으)로 실행했습니다."
+                }
     }
 }
--- app/src/main/java/com/hegunhee/fcmstudy/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/hegunhee/fcmstudy/MyFirebaseMessagingService.kt
@@ -3,9 +3,12 @@ package com.hegunhee.fcmstudy
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.util.Log
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -20,18 +23,19 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
 
-        Log.d("Test","From: " + remoteMessage!!.from)
+        Log.d("Test", "From: " + remoteMessage!!.from)
 
-        if(remoteMessage.data.isNotEmpty()){
+        if (remoteMessage.data.isNotEmpty()) {
             val data = remoteMessage.data
-            Log.d("바디: ",remoteMessage.data.toString())
-            Log.d("바디?","hello1 ")
-            Log.d("타이틀: ",data["title"] ?: "타이틀 찾지못함")
-            Log.d("메세지: ",data["message"] ?: "메세지도 찾지못함")
-            Log.d("바디?","hello2 ")
+            Log.d("바디: ", remoteMessage.data.toString())
+            Log.d("바디?", "hello1 ")
+            Log.d("타이틀: ", data["title"] ?: "타이틀 찾지못함")
+            Log.d("메세지: ", data["message"] ?: "메세지도 찾지못함")
+            Log.d("바디?", "hello2 ")
 
 
             createNotificationChannel()
+
 
             val type = remoteMessage.data["type"]?.let { NotificationType.valueOf(it) }
             val title = remoteMessage.data["title"]
@@ -41,56 +45,83 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
 
             NotificationManagerCompat.from(this)
-                .notify(type.id,createNotification(type,title,message))
-        }else{
-            Log.d("수신에러: ","data가 비어있습니다. 메시지를 수신하지 못했습니다.")
+                .notify(type.id, createNotification(type, title, message))
+        } else {
+            Log.d("수신에러: ", "data가 비어있습니다. 메시지를 수신하지 못했습니다.")
             Log.d("data 값: ", remoteMessage.data.toString())
         }
     }
 
-    private fun createNotification(type : NotificationType,title : String?, message : String?): Notification{
+    private fun createNotification(
+        type: NotificationType,
+        title: String?,
+        message: String?
+    ): Notification {
+        val intent = Intent(this,MainActivity::class.java).apply {
+            putExtra("notificationType","${type.title} 타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+        val pendingIntent = PendingIntent.getActivities(this,type.id,arrayOf<Intent>(intent), PendingIntent.FLAG_UPDATE_CURRENT)
         val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_baseline_notifications_24)
             .setContentTitle(title)
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
 
-        when(type){
+        when (type) {
             NotificationType.NORMAL -> Unit
             NotificationType.EXPANDABLE -> {
                 notificationBuilder.setStyle(
                     NotificationCompat.BigTextStyle()
                         .bigText(
-                            "BIGSTYLE"
+                            "😀 😃 😄 😁 😆 😅 😂 🤣 🥲 ☺️ 😊 😇 " +
+                                    "🙂 🙃 😉 😌 😍 🥰 😘 😗 😙 😚 😋 😛 " +
+                                    "😝 😜 🤪 🤨 🧐 🤓 😎 🥸 🤩 🥳 😏 😒 " +
+                                    "😞 😔 😟 😕 🙁 ☹️ 😣 😖 😫 😩 🥺 😢 " +
+                                    "😭 😤 😠 😡 🤬 🤯 😳 🥵 🥶 😱 😨 😰 " +
+                                    "😥 😓 🤗 🤔 🤭 🤫 🤥 😶 😐 😑 😬 🙄 " +
+                                    "😯 😦 😧 😮 😲 🥱 😴 🤤 😪 😵 🤐 🥴 " +
+                                    "🤢 🤮 🤧 😷 🤒 🤕"
                         )
                 )
             }
             NotificationType.CUSTOM -> {
-
+                notificationBuilder.setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply {
+                            setTextViewText(R.id.title, title)
+                            setTextViewText(R.id.message, message)
+                        }
+                    )
             }
         }
         return notificationBuilder.build()
     }
-    private fun createNotificationChannel(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 CHANNEL_ID,
                 CHANNEL_NAME,
                 NotificationManager.IMPORTANCE_DEFAULT
             )
-            channel.description = CHANNEL_NAME
+            channel.description = CHANNEL_DESC
+
             (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
                 .createNotificationChannel(channel)
         }
     }
-    companion object{
+
+    companion object {
         const val CHANNEL_ID = "id"
         const val CHANNEL_NAME = "name"
         const val CHANNEL_DESC = "채널 설명입니다."
     }
 
-    private fun sendNotification(remoteMessage: RemoteMessage){
-
-    }
 
 }
